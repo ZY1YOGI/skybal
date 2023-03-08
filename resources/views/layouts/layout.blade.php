55f1866b68df8fa10cecb79588fc4f4a425d549b
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-03-08 20:27:15 +0200

    mange file layouts and add a link icon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,23 +12,20 @@
   <meta name="keywords" content="{{ $keywords }}" />
   {{-- Title --}}
   <title>{{ $title }}</title>
-
+  {{-- Icon --}}
+  <link rel="shortcut icon" href={{ Vite::asset('resources/image/logo.svg') }} type="image/x-icon" />
   {{-- Fonts --}}
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" />
-
   {{-- AOS --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
-
   {{-- AlpineJs --}}
   <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
-
   {{-- Scripts --}}
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-
   @include('layouts.includes.header')
   {{ $slot }}
   @include('layouts.includes.footer')
